9f629c43044d10211cd0df1c349f3f59ede082af
David Green <pbcub1.dg@gmail.com>
2016-07-01 19:06:44 -0600

    Basic HTML Structure for home page
    Added:
    App PHP class
    index.php HTML Structure

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+	class App{
+		$appName = "Social networking website";
+	}
+
+	$app = new App();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title><?php echo $app->appName;?></title>
+	</head>
+	<body></body>
+</html>
