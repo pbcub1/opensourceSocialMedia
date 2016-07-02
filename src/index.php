7e669493356fc6fe2ccf1b38f0c0de299a248d8a
David Green <pbcub1.dg@gmail.com>
2016-07-01 19:48:56 -0600

    Modifications to main App class
    Added:
    Constructor pseudo code for App class
    app version constant for App class
    require call to lib/db.php (Does not exist yet)
    
    Modified:
    App Class

--- src/index.php
+++ src/index.php
@@ -1,6 +1,37 @@
 <?php
+	//This is where the database functions are stored.
+
+	require 'lib/db.php';
 	class App{
-		$appName = "Social networking website";
+		//Constants
+		public const $appName = "Social networking website";
+		public const $appVersion = 0.0.1
+
+		//Constructor
+		public __construct() {
+			//Open database
+			//if session profile variable exist
+				//get session entry based off session ID
+				//if not expired
+					//if type is session
+						//generate feed page
+					//else
+						//generate logon page
+				//else
+					//generate logon page
+			//else if cookie variables exist
+				//get session entry based off cookie's session ID
+				//if not expired
+					//if type is cookie
+						//create session variables
+						//generate feed page
+					//else
+						//generate logon page
+				//else
+					//generate logon page
+			//else
+				//generate logon page
+		}
 	}
 
 	$app = new App();
